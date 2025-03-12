2992872bd78a655df808ead3cbd7137dbf19e4e0
DevAlejandro2007 <alejandrorojasbenitez2018@gmail.com>
2025-03-12 17:50:47 -0500

    trabajo_final

--- gestion_notas.py
+++ gestion_notas.py
@@ -1,13 +1,21 @@
 estudiantes = []
 notas_estudiantes = {}
 
+def continuar():
+    input("PRECIONA ENTER PARA CONTINUAR")
+    return
+
+def nombre():
+    estudi = input("NOMBRE DEL ESTUDIANTE AL QUE SE DESEA VER NOTAS \n").upper()
+    return estudi
+
 def inicio(): 
     print("""
     --------------------
     ---SOFTWARE NOTAS---
     --------------------
           """)
-    input("OPRIME ENTER PARA EMPEZAR")
+    continuar()
     menu()
 
 def menu():
@@ -17,7 +25,7 @@ def menu():
     ---------------
     """)
 
-    menu = int(input("""
+    op = int(input("""
     1. INSERTAR NOTAS
     2. VER NOTAS DE ESTUDIANTE
     3. EDITAR NOTAS DE ESTUDIANTE
@@ -25,44 +33,63 @@ def menu():
                  
     ESCRIBE LA OPCION QUE DECEE EJECUTAR
     """))
-    if menu == 1:
+    if  op == 1:
         notas()
+    elif op == 2:
+        ver_notas()
+    elif op == 3:
+        editar_nota()
+    elif op == 4:
+        print()
+        print("**GRACIAS POR USAR EL SOFTWARE**")
+        print()
+        continuar()
+    else:
+        print("***OPCION INCORRECTA***")
+        menu()
 
 
 def notas():
     print()
-    profe = input("NOMBRE DEL PROFESOR QUE INSERTARA NOTAS \n")
-    print()
-    estudi = input("NOMBRE DEL ESTUDIANTE AL QUE SE INSERTARA NOTAS \n")
+    estudi = nombre()
     estudiantes.append(estudi)
     agregar_nota = int(input("CUANTAS NOTAS VAS A INGREAS"))
     total_nota = []
     for i in range(1, agregar_nota + 1):
         nota = float(input(f"NOTA {i} = "))
         total_nota.append(nota)
-        notas_estudiantes[estudi] = profe
     notas_estudiantes[estudi] = total_nota 
-    
-    
     print(notas_estudiantes)
+    continuar()
     menu()
-    print()
-
-        
-        
-
-
-        
-        
-
-
-  
     
 
+def ver_notas():
+    print()
+    estudi = nombre()
+    if estudi in estudiantes:
+        print(f"***LAS NOTAS DEL ESTUDIANTE {estudi} SON : ***")
+        print(notas_estudiantes[estudi])
+        print()
+    else:
+        print("ESTE ESTUDIANTE NO EXISTE")
+    continuar()
+    menu()
 
-
-
-
-
+def editar_nota():
+    print()
+    estudi = nombre()
+    print(F"**EL ESTUDIANTE TIENE LAS SIGUIENTES NOTAS**")
+    print(notas_estudiantes[estudi])
+    cutos_cambios = int(input("CUANTOS CAMBIOS VAS A REALIZAR"))
+    for i in range(1, cutos_cambios + 1):
+        print(f"**CAMBIO {i}**")
+        cambio = int((input("**QUE POSCISION DE LA NOTA QUIERES EDITAR**")))
+        nota = float(input("QUE NOTA QUIERES INSERTAR"))
+        notas_estudiantes[estudi][cambio - 1] = nota
+        print("**CAMBIO REALIZADO**")
+        print(notas_estudiantes[estudi])
+        continuar()
+    menu()
 
 inicio()
